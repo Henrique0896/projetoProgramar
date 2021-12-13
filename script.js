030b12ed0abc10c662f49401847ceb5981365982
Henrique Lima <barrosohenriquelima@gmail.com>
2021-12-13 18:11:47 -0300

    funções editar

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function salvarDados(tiposDados, dados) {
+function salvarDados(tipoDados, dados) {
     dados = JSON.stringify(dados)
-    localStorage.setItem(tiposDados, dados);
+    localStorage.setItem(tipoDados, dados);
 }
 
 function lerDados(tipoDados) {
-    dados = []
+    dados = null
     if (localStorage.getItem(tipoDados)) {
         dados = localStorage.getItem(tipoDados)
         dados = JSON.parse(dados)
@@ -15,24 +15,18 @@ function lerDados(tipoDados) {
 
 function mostrarSanduiches() {
     var area = document.getElementById("sanduiches")
-
     sanduiches = lerDados("sanduiches")
-
     html = ""
 
-    sanduiches.forEach((sanduiche) => {
-
-        html += `id: ${sanduiche.id}
-  
-   
+    if (sanduiches != null) {
+        sanduiches.forEach((sanduiche) => {
+            html += `id: ${sanduiche.id}
         Nome: ${sanduiche.nome}
-   
         Composicao: ${sanduiche.composicao}
-      
         Preço: ${sanduiche.preco}  <br/>`
 
-    })
-    console.log(html)
+        })
+    }
 
     area.innerHTML = html
 }
@@ -40,19 +34,42 @@ function mostrarSanduiches() {
 function criarSanduiche() {
 
     var sanduiches = lerDados("sanduiches");
-
-    var id = 1
+    var id = lerDados("id")
     var nome = prompt("Qual o nome do sanduiche?")
     var composicao = prompt("Qual a composição:")
     var preco = prompt("Qual o preco")
 
+    if (id == null) {
+        id = 1
+    } else {
+        id += 1
+    }
+    salvarDados("id", id)
     var novoSanduiche = { id: id, nome: nome, composicao: composicao, preco: preco }
-
+    if (sanduiches == null) {
+        sanduiches = []
+    }
     sanduiches.push(novoSanduiche)
-
     salvarDados("sanduiches", sanduiches)
-
     mostrarSanduiches()
 
 }
 
+function editar(id) {
+    sanduiches = lerDados("sanduiches")
+    if (sanduiches == null) {
+        sanduiches = []
+    }
+
+    sanduiches.forEach((sanduiche) => {
+
+        if (sanduiche.id == id) {
+            sanduiche.nome = prompt("Novo nome:")
+            sanduiche.composicao = prompt("Nova Composicao:")
+            sanduiche.preco = prompt("Novo preco:")
+        }
+    })
+
+    salvarDados("sanduiches", sanduiches)
+    mostrarSanduiches()
+}
